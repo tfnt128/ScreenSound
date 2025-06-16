d360361c3f6b4b35f6c19cee3a7b0edc3220dd8c
Thiago Fontoura <thiago.fontoura120@gmail.com>
2025-06-16 18:52:31 -0300

    Adição do Bando de Dados músicas na API e todas as funcionalidades já implementadas

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
 
 builder.Services.Configure
     <Microsoft.AspNetCore.Http.Json.JsonOptions>
@@ -56,12 +57,62 @@ app.MapPut("/Artistas", ([FromBody] Artista artista, [FromServices] DAL<Artista>
     artistaAtualizado.Nome = artista.Nome;
     artistaAtualizado.Bio = artista.Bio;
     artistaAtualizado.FotoPerfil = artista.FotoPerfil;
-    artistaAtualizado.Id = artista.Id;
 
     dal.Update(artistaAtualizado);
 
     return Results.Ok(artista);
 });
+
+
+app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) => {
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", (string nome, [FromServices] DAL<Musica> dal) => {
+    var musica = dal.Recuperar(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (musica == null)
+    {
+        return Results.NotFound($"Artista '{nome}' não encontrado.");
+    }
+    return Results.Ok(musica);
+});
+
+app.MapPost("/Musicas", ([FromBody] Musica musica, [FromServices] DAL<Musica> dal) => {
+    dal.Add(musica);
+    return Results.Ok(musica);
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
+    var musica = dal.Recuperar(a => a.Id == id);
+    if (musica == null)
+    {
+        return Results.NotFound($"Música com o ID: '{id}' não encontrado.");
+    }
+    dal.Delete(musica);
+    return Results.Ok(musica);
+});
+
+app.MapPut("/Musicas", ([FromBody] Musica musica, [FromServices] DAL<Musica> dal) => {
+    var musicaAtualizado = dal.Recuperar(a => a.Id == musica.Id);
+    if (musicaAtualizado == null)
+    {
+        musicaAtualizado = dal.Recuperar(a => a.Nome.ToUpper().Equals(musica.Nome.ToUpper()));
+        if (musicaAtualizado == null)
+        {
+            return Results.NotFound($"Música com o ID: {musica.Id} ou com o nome '{musica.Nome}' não encontrado.");
+        }
+
+    }
+    musicaAtualizado.Nome = musica.Nome;
+    musicaAtualizado.ReleaseYear = musica.ReleaseYear;
+    musicaAtualizado.Artist = musica.Artist;
+
+    dal.Update(musicaAtualizado);
+
+    return Results.Ok(musica);
+});
+
+
 
 
 
