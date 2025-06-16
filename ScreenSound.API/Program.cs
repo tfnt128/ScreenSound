ba841e3b989dfbba83c21bb265d702a97fc10c03
Thiago Fontoura <thiago.fontoura120@gmail.com>
2025-06-16 17:56:55 -0300

    Funcionalidade de Remove e Update adicionadas na API

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -22,7 +22,7 @@ app.MapGet("/Artistas/{nome}", (string nome, [FromServices] DAL<Artista> dal) =>
     var artista = dal.Recuperar(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
     if (artista == null)
     {
-        return Results.NotFound($"Artista {nome} não encontrado.");
+        return Results.NotFound($"Artista '{nome}' não encontrado.");
     }
     return Results.Ok(artista);
 });
@@ -31,5 +31,38 @@ app.MapPost("/Artistas", ([FromBody]Artista artista, [FromServices] DAL<Artista>
     dal.Add(artista);
     return Results.Ok(artista);
 });
+
+app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) => {
+    var artista = dal.Recuperar(a => a.Id == id);
+    if (artista == null)
+    {
+        return Results.NotFound($"Artista com o ID: '{id}' não encontrado.");
+    }
+    dal.Delete(artista);
+    return Results.Ok(artista);
+});
+
+app.MapPut("/Artistas", ([FromBody] Artista artista, [FromServices] DAL<Artista> dal) => {
+    var artistaAtualizado = dal.Recuperar(a => a.Id == artista.Id);
+    if (artistaAtualizado == null)
+    {
+        artistaAtualizado = dal.Recuperar(a => a.Nome.ToUpper().Equals(artista.Nome.ToUpper()));
+        if (artistaAtualizado == null)
+        {
+            return Results.NotFound($"Artista com o ID: {artista.Id} ou com o nome '{artista.Nome}' não encontrado.");
+        }
+            
+    }
+    artistaAtualizado.Nome = artista.Nome;
+    artistaAtualizado.Bio = artista.Bio;
+    artistaAtualizado.FotoPerfil = artista.FotoPerfil;
+    artistaAtualizado.Id = artista.Id;
+
+    dal.Update(artistaAtualizado);
+
+    return Results.Ok(artista);
+});
+
+
 
 app.Run();
